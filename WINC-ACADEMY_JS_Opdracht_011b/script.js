2edd6aba71fa415d916c2508ac68a6c89079dd33
malutonnaer <81236935+malutonnaer@users.noreply.github.com>
2021-05-12 13:56:26 +0200

    Add files via upload

---
+++ WINC-ACADEMY_JS_Opdracht_011b/script.js
@@ -0,0 +1,16 @@
+// for (let i = 0; i <= 10; i++) {  
+//     console.log(i * 9);
+// };
+
+// const multiplier = 9;
+// for (let i = 0; i <= 10; i++) {
+//   let result = multiplier * i;
+//   console.log(multiplier + " * " + i + " = " + result);
+// }
+
+for (let multiplier = 0; multiplier <= 10; multiplier++) {
+  for (let i = 0; i <= 10; i++) {
+    let result = multiplier * i;
+    console.log(multiplier + " * " + i + " = " + result);
+  }
+};
